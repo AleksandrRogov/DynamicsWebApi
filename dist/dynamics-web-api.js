d26ee497ee7be47e6a2f6847136820ce51c82f28
AleksandrRogov <lex.o4u@gmail.com>
2017-11-06 21:59:25 -0500

    Release v1.3.2

--- dist/dynamics-web-api.js
+++ dist/dynamics-web-api.js
@@ -1,4 +1,4 @@
-/*! dynamics-web-api v1.3.1 (c) 2017 Aleksandr Rogov */
+/*! dynamics-web-api v1.3.2 (c) 2017 Aleksandr Rogov */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
