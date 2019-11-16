d88cf0d577c0b39f7a813d97902b518bf9aff6c9
AleksandrRogov <lex.o4u@gmail.com>
2019-11-16 10:55:54 -0500

    Release v1.5.14

--- dist/dynamics-web-api-callbacks.js
+++ dist/dynamics-web-api-callbacks.js
@@ -1,4 +1,4 @@
-/*! dynamics-web-api-callbacks v1.5.13 (c) 2019 Aleksandr Rogov */
+/*! dynamics-web-api-callbacks v1.5.14 (c) 2019 Aleksandr Rogov */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
@@ -2853,7 +2853,9 @@ function parseBatchResponse(response, parseParams, requestNumber) {
                         var entityUrl = /OData-EntityId.+/i.exec(batchResponse);
 
                         if (entityUrl && entityUrl.length) {
-                            result.push(/([0-9A-F]{8}[-]?([0-9A-F]{4}[-]?){3}[0-9A-F]{12})\)$/i.exec(entityUrl[0])[1]);
+                            var guidResult = /([0-9A-F]{8}[-]?([0-9A-F]{4}[-]?){3}[0-9A-F]{12})\)$/i.exec(entityUrl[0]);
+
+                            result.push(guidResult ? guidResult[1] : undefined);
                         }
                         else {
                             result.push(undefined);
@@ -2910,9 +2912,10 @@ module.exports = function parseResponse(response, responseHeaders, parseParams) 
             }
     }
 
-    return parseResult;
-};
+    parseParams.length = 0;
 
+    return parseResult;
+}
 
 /***/ }),
 /* 9 */
